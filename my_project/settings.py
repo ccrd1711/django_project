7e3df2ac0a28ea057202b79411dbba4189ccba5f
ccrd1711 <ccrd17@gmail.com>
2025-05-19 07:34:10 +0100

    Debug change

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xr4i&)8b753m0v#&=1x8nma0gtwl!owzls2ym(e^j_+48-#*23'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
